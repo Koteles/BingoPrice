681bb87dca53c184fded7ba91f9b6d1f26268c30
lab2 <grigore.sorin@outlook.com>
2016-12-08 18:09:18 +0200

    New

--- app/src/main/java/com/example/android/bingoprice/MainActivity.java
+++ app/src/main/java/com/example/android/bingoprice/MainActivity.java
@@ -153,8 +153,9 @@ public class MainActivity extends AppCompatActivity {
 
             Intent intent = new Intent(Intent.ACTION_SENDTO);
             intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-            intent.putExtra(Intent.EXTRA_EMAIL, "koteles.krisztian@yahoo.ro");
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"koteles.krisztian@yahoo.ro"});
             intent.putExtra(Intent.EXTRA_SUBJECT, "BingoPrice");
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Pretul nou este: " + price);
             if (intent.resolveActivity(getPackageManager()) != null) {
                 startActivity(intent);
             }
